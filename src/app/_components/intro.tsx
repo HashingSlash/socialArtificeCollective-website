eaff3aef79d5016c6d9fcdfb07bbcae8bf412ad2
HashingSlash <dcjpreece@protonmail.com>
2025-08-23 15:54:39 +0800

    headshots added to intro

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,12 +4,10 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex md:justify-between mt-12 mb-12 md:mb-10">
       <h4 className="text-center md:text-left text-sml mt-5 md:pl-8">
-        Welcome to the home of Social Artifice Collective's work.
-      </h4>
-      <img src="/assets/blog/authors/sacbanner.png"/>
-      <h4 className="text-center md:text-right text-sml mt-5 md:pl-8">
         "I'm gonna need another universe, I tore mine apart." Everytime I Die - C++
       </h4>
+      <img src="/assets/blog/authors/sacbannernoborder.png"/>
+      <img src="/assets/blog/authors/headshots.png"/>
     </section>
   
   );
